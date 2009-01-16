8475e29abe76f6fa9a8c45476ba11a5e03dca363
Pablo Brasero <pablo.brasero@pearlmedical.co.uk>
2009-01-16 16:24:57 +0000

    Convert escaped Unicode characters to HTML entities.

--- lib/bossman/basevalueobject.rb
+++ lib/bossman/basevalueobject.rb
@@ -2,6 +2,7 @@ module BOSSMan
   class BaseValueObject
 
       def set_parameter(key, value)
+        value = value.gsub(/\\u(\d+)/, '&#x\1;') if value.kind_of? String
         instance_variable_set("@#{key}", value) 
         instance_eval("def #{key}; @#{key}; end")
       end
